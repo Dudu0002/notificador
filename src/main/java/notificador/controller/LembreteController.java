e96108d7661c1a93e4e8d296eecc92b8ef2de371
Eduardo Henrique Souza Silva <eduardo.silva@infive.com.br>
2026-04-22 16:02:14 -0300

    Envia task 5

--- src/main/java/notificador/controller/LembreteController.java
+++ src/main/java/notificador/controller/LembreteController.java
@@ -26,19 +26,20 @@ public class LembreteController {
 	    System.out.println(data);
 
 	    String title = (String) data.get("title");
+	    String priority = (String) data.get("priority");
 
+	    if (priority == null) {
+	        priority = "sem prioridade";
+	    }
 
-	    enviarWhatsApp(
-	        "5519999273962",
-	        "📝 Nova tarefa criada: " + title
-	    );
+	    enviarWhatsApp(title, priority);
 
 	    return ResponseEntity.ok("Task recebida e enviada ao WhatsApp");
 	}
 
 	public void enviarWhatsApp(String title, String priority) {
 
-	    String url = "https://api.z-api.io/instances/SEU_INSTANCE/token/SEU_TOKEN/send-text";
+	    String url = "https://api.z-api.io/instances/3F20855B43F1D1DA89CB9A962EE6827F/token/AE1078FF9A133EE7F416D99A/send-text";
 
 	    Map<String, Object> body = new HashMap<>();
 	    body.put("phone", "5511999999999");
@@ -46,7 +47,7 @@ public class LembreteController {
 
 	    HttpHeaders headers = new HttpHeaders();
 	    headers.setContentType(MediaType.APPLICATION_JSON);
-	    headers.set("Client-Token", "SEU_CLIENT_TOKEN");
+	    headers.set("Client-Token", "F396f2b9b7de742aaa37650dd2456f497S");
 
 	    HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
 
